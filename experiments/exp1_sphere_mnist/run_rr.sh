b6ffb40b809537298148da7b7b8523eb716dab4b
Ren Komatsu <komatsu@robot.t.u-tokyo.ac.jp>
2020-03-06 17:28:30 +0900

    create run file for nn, nr, and rr

---
+++ experiments/exp1_sphere_mnist/run_rr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#source activate
+MESHFILES=../../mesh_files
+# DATAFILE=mnist_ico5.zip
+DATAFILE=mnist_ico4_rr.zip
+
+# assert mesh files exist
+if [ ! -d $MESHFILES ]; then
+    echo "[!] Mesh files do not exist..."
+    exit
+fi
+
+# generate data
+if [ ! -f $DATAFILE ]; then
+    echo "[!] Data files do not exist. Preparing data..."
+    python prepare_data.py --bandwidth 60 --mnist_data_folder raw_data --direction NP --output_file $DATAFILE  --mesh_file $MESHFILES/icosphere_4.pkl
+fi
+
+# train
+python train.py --mesh_folder $MESHFILES --datafile $DATAFILE --log_dir log_rr --optim adam --lr 1e-2 --epochs 50 --feat 16 --decay --batch-size 32
